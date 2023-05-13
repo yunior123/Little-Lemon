0f6f52c77b033e1dda5ea1d4a3e09d7a11c5b066
yunior123 <yuniorrodriguezo460@gmail.com>
2023-05-12 23:23:32 -0400

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Yunior Rodriguez Osorio on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Little LemonUITests/Little_LemonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Little_LemonUITestsLaunchTests.swift
+//  Little LemonUITests
+//
+//  Created by Yunior Rodriguez Osorio on 12/5/23.
+//
+
+import XCTest
+
+final class Little_LemonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
